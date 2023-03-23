bd9a924d6d41f0c76ef866cf4b47ce9b1854f70d
simon_no <shiamson@hotmail.com>
2023-03-23 12:16:31 +0530

    Finished JS challenge

--- js/script.js
+++ js/script.js
@@ -1,11 +1,14 @@
 var tripInfoButton = document.querySelector(".trip-info");
-var dailyBudget = document.querySelector("daily-budget");
+var dailyBudget = document.querySelector(".daily-budget");
 var tripInfo = function () {
     var totalBudget = Number(prompt("What is your total budget?"));
     var accommodation = Number(prompt("What are your accommodation costs?"));
     var numDays = Number(prompt("How many days will your trip last?"));
+    calculateDailyBudget(totalBudget,accommodation,numDays);
 };
+
 var calculateDailyBudget = function (totalBudget, accommodation, numDays) {
-    var daily = ((totalBudget - accommodation) / numDays)toFixed(2);
+    var daily = ((totalBudget - accommodation) / numDays).toFixed(2);
+    dailyBudget.innerText = `You can spend $${daily} a day on food and fun.`;
 };
-dailyBudget.innerText = "You can spend $${daily} a day on food and fun.";
+tripInfoButton.addEventListener("click", tripInfo);
