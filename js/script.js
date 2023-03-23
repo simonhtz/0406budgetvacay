d8c480c1b6b87c1140b6de039c67da6a4b65bdb8
simon_no <shiamson@hotmail.com>
2023-03-23 11:55:43 +0530

    Added JS

---
+++ js/script.js
@@ -0,0 +1,11 @@
+var tripInfoButton = document.querySelector(".trip-info");
+var dailyBudget = document.querySelector("daily-budget");
+var tripInfo = function () {
+    var totalBudget = Number(prompt("What is your total budget?"));
+    var accommodation = Number(prompt("What are your accommodation costs?"));
+    var numDays = Number(prompt("How many days will your trip last?"));
+};
+var calculateDailyBudget = function (totalBudget, accommodation, numDays) {
+    var daily = ((totalBudget - accommodation) / numDays)toFixed(2);
+};
+dailyBudget.innerText = "You can spend $${daily} a day on food and fun.";
